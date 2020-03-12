1f27b59537ca0efede5dd206660922721f8e7c93
Fletcher Kelly <flkelly@microsoft.com>
2020-03-12 18:38:41 +0200

    AKS Script update 👍

--- aks/aksWorkshop/createLabAKS.sh
+++ aks/aksWorkshop/createLabAKS.sh
@@ -1,26 +1,29 @@
 az login
-SUB_ID=""
-REGION=""
-RG_NAME=""
-AKS_CLUSTER=""
-AKS_SPN=""
-AD_APP_NAME=""
+SUB_ID="949ef534-07f5-4138-8b79-aae16a71310c"
+REGION="westeurope"
+RG_NAME="flkelly-weu-rg1"
+AKS_CLUSTER="flkelly-weu-rg1-aks"
+AKS_SPN="aksspn-fl2"
+AD_APP_NAME="aksapp02"
 
 az account set --subscription ${SUB_ID}
-az ad app create --display-name ${AD_APP_NAME} --homepage "http://localhost/${AD_APP_NAME}" --identifier-uris [http://localhost/${AD_APP_NAME}](http://localhost/${AD_APP_NAME})
-AD_APP_ID=$(az ad app list --display-name ${AD_APP_NAME} --query [].appId -o tsv)
-SPN_PASSWORD="blahfishpaste"
-az ad sp create-for-rbac --name ${AD_APP_NAME} --password ${SPN_PASSWORD}
 
 az aks get-versions -l ${REGION} -o table
 kubernetesVersionLatest=$(az aks get-versions -l ${REGION} --query 'orchestrators[-1].orchestratorVersion' -o tsv)
 
-az ad sp create-for-rbac --name ${AKS_SPN} --output table
+SPN=$(az ad sp create-for-rbac --name ${AKS_SPN})
 AKS_APP_ID=$(az ad app list --display-name ${AKS_SPN} --query [].appId -o tsv)
+
+##needed to get SPN PWD
+sudo apt-get install jq
+
+SPN_PWD=$(echo "${SPN}" | jq -c '.password')
+SPN_PWD=$(echo ${SPN_PWD%?} | cut -c2-)
+
 az group create --name ${RG_NAME} --location ${REGION}
-kubectl create clusterrolebinding kubernetes-dashboard -n kube-system --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
-az aks create --resource-group akschallenge --name ${AKS_CLUSTER}--enable-addons monitoring --kubernetes-version $kubernetesVersionLatest --generate-ssh-keys --location ${REGION} --service-principal ${AD_APP_ID} --client-secret ${SPN_PASSWORD}
-#az aks create --resource-group ${RG_NAME} --name ${AKS_CLUSTER} --enable-addons monitoring --generate-ssh-keys --location ${REGION} --kubernetes-version $kubernetesVersionLatest
+az aks create --resource-group ${RG_NAME} --name ${AKS_CLUSTER} --enable-addons monitoring --kubernetes-version $kubernetesVersionLatest --generate-ssh-keys --location ${REGION} --service-principal ${AKS_APP_ID} --client-secret ${SPN_PWD}
+
 az aks install-cli 
 az aks get-credentials --resource-group ${RG_NAME} --name ${AKS_CLUSTER}
+kubectl create clusterrolebinding kubernetes-dashboard -n kube-system --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
 kubectl get nodes
